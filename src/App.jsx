6991f445073e647128a03343dd82e436d2ea20b7
kareem <kareem.gamail>
2026-04-07 03:29:21 +0200

    modfire image

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <section className="retative flex items-center min-h-screen w-screen">
         <img
-          src="./src/assets/bg.jpg"
+          src="src/assets/bg.jpg"
           alt="hero_image"
           className="absolute inset-0 h-full w-full object-cover"
         />
